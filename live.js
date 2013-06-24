6c0b2099a7a0c70a561fba090ed3804b5cd7d699
atsneves <atsneves@gmail.com>
2013-06-24 04:09:15 -0300

    Change Server

--- live.js
+++ live.js
@@ -10,14 +10,14 @@ var mongoose = require("mongoose");
 
 var authUrl = graph.getOauthUrl({
     "client_id":     "467592886661340"
-  , "redirect_uri":  "http://ec2-54-218-210-177.us-west-2.compute.amazonaws.com:2400//facebook/logged"
+  , "redirect_uri":  "http://ec2-54-218-210-177.us-west-2.compute.amazonaws.com:2400/facebook/logged"
 });
 
 var conf = {
 	client_id:      '467592886661340'
 	, client_secret:  '61d508188ece22dcdb67aceed8794d24'
 	, scope:          'email, user_about_me, user_birthday, user_location, publish_stream'
-	, redirect_uri:   'http://ec2-54-218-210-177.us-west-2.compute.amazonaws.com:2400//facebook/logged'
+	, redirect_uri:   'http://ec2-54-218-210-177.us-west-2.compute.amazonaws.com:2400/facebook/logged'
 };
 
 // after user click, auth `code` will be set
